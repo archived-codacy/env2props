deb390efdffb41f85bb7609e17c7af0758cf6d28
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2019-02-14 19:38:06 +0000

    test: Add tests using Typesafe config and string with spaces

--- spec/java_properties_spec.cr
+++ spec/java_properties_spec.cr
@@ -11,20 +11,42 @@ def run_cmd(cmd, args)
   end
 end
 
-cmd = "sh"
+CMD          = "sh"
+DEFAULT_ARGS = ["-c"]
 
 it "should properly propagate simple java properties" do
-  args = ["-c", %(export CONFIG_mykey=myvalue && java "$(bin/env2props -p CONFIG_)" spec.CheckProperties)]
-  exit_value, output = run_cmd(cmd, args)
+  args = DEFAULT_ARGS.clone
+  args << %(export CONFIG_mykey1=myvalue && java $(bin/env2props -p CONFIG_) spec.CheckProperties)
+  exit_value, output = run_cmd(CMD, args)
 
-  output.includes?("mykey:myvalue").should eq(true)
+  output.includes?("mykey1:myvalue").should eq(true)
   exit_value.should eq(0)
 end
 
 it "should properly propagate escaped multi string java properties" do
-  args = ["-c", %(export CONFIG_mykey="my value" && java "$(bin/env2props -p CONFIG_)" spec.CheckProperties)]
-  exit_value, output = run_cmd(cmd, args)
+  args = DEFAULT_ARGS.clone
+  args << %(export CONFIG_mykey2="my value" && sh -c "java $(bin/env2props -p CONFIG_) spec.CheckProperties")
+  exit_value, output = run_cmd(CMD, args)
 
-  output.includes?("mykey:\"my value\"").should eq(true)
+  output.includes?("mykey2:my value").should eq(true)
+  exit_value.should eq(0)
+end
+
+it "should properly inject multiple java properties" do
+  args = DEFAULT_ARGS.clone
+  args << %(export CONFIG_mykey11=1 && export CONFIG_mykey22=2 && java $(bin/env2props -p CONFIG_) spec.CheckProperties)
+  exit_value, output = run_cmd(CMD, args)
+
+  output.includes?("mykey11:1").should eq(true)
+  output.includes?("mykey22:2").should eq(true)
+  exit_value.should eq(0)
+end
+
+it "should get unquoted strings when using Typesafe config" do
+  args = DEFAULT_ARGS.clone
+  args << %(export CONFIG_mykey="1 2 3" && sh -c "java $(bin/env2props -p CONFIG_) -cp config-1.3.2.jar:. spec.CheckTypesafeConfig")
+  exit_value, output = run_cmd(CMD, args)
+
+  output.should eq("1 2 3")
   exit_value.should eq(0)
 end
